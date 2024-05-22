633d9f2cfd58abfb2e85c255e319754b73cab087
sp77f <spyf77@gmail.com>
2024-05-22 16:06:43 +0300

    add saving n loading

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pickle
 class Animal (object):
     def __init__(self, name, age):
         self.name = name
@@ -75,6 +76,17 @@ class Zoo():
         self.veterinarians.append(veterinarian)
         print(f"Сотрудник {veterinarian.name} приписан к зоопарку {self.name}")
 
+    def save_zoo(self):
+        with open('zoo_data.pkl', 'wb') as file:
+            pickle.dump(self, file)
+
+    def load_zoo():
+        try:
+            with open('zoo_data.pkl', 'rb') as file:
+                return pickle.load(file)
+        except FileNotFoundError:
+            return None
+
 
 def animal_eat(animals):
     for animal in animals:
@@ -83,24 +95,25 @@ def animal_make_sound(animals):
     for animal in animals:
         print (animal.make_sound())
 
-
-zoo_list = []
-zoo_keepers = []
-veterinarians = []
-zookeeper1 = Zoo_keeper("Саша", 30, 5, 10000)
-veterinarian1 = Veterinarian("Витя", 25, 3, "Высшее")
-zoo1 = Zoo("Зоопарк", "Москва", zoo_list, zoo_keepers, veterinarians)
-zoo_list.append(Bird("Ворона", 5, "Всё", "Кар-кар"))
-zoo_list.append(Mammal("Корова", 5, "Трава", "Му"))
-zoo_list.append(Reptile("Кобра", 5, "Мыши", "Ш-ш-ш"))
-zoo1.add_animal(zoo_list[0])
-zoo1.add_animal(zoo_list[1])
-zoo1.add_animal(zoo_list[2])
-zoo1.add_keeper(zookeeper1)
-zoo1.add_veterinarian(veterinarian1)
+zoo1 = Zoo.load_zoo()
+# zoo_list = []
+# zoo_keepers = []
+# veterinarians = []
+# zookeeper1 = Zoo_keeper("Саша", 30, 5, 10000)
+# veterinarian1 = Veterinarian("Витя", 25, 3, "Высшее")
+# zoo1 = Zoo("Зоопарк", "Москва", zoo_list, zoo_keepers, veterinarians)
+# zoo_list.append(Bird("Ворона", 5, "Всё", "Кар-кар"))
+# zoo_list.append(Mammal("Корова", 5, "Трава", "Му"))
+# zoo_list.append(Reptile("Кобра", 5, "Мыши", "Ш-ш-ш"))
+# zoo1.add_animal(zoo_list[0])
+# zoo1.add_animal(zoo_list[1])
+# zoo1.add_animal(zoo_list[2])
+# zoo1.add_keeper(zookeeper1)
+# zoo1.add_veterinarian(veterinarian1)
 
 animal_eat(zoo1.animals)
 animal_make_sound(zoo1.animals)
 
 zoo1.veterinarians[0].heal_animals(zoo1.animals[0])
 zoo1.keepers[0].feed_animals(zoo1.animals[0])
+zoo1.save_zoo()
